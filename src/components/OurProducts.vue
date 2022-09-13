602b3f6a5017a0b269a40f7143a38e413845011f
yigithanoguz <yigithanoguz28@gmail.com>
2022-09-13 23:59:28 +0300

    our products finish

--- src/components/OurProducts.vue
+++ src/components/OurProducts.vue
@@ -17,45 +17,58 @@
           <a> Tables </a>
         </li>
       </ul>
-      <div class="slider">
-        <div class="card" v-for="(item, index) in products" :key="index">
-          <div class="product-image">
-            <img :src="item.imageLink" alt="..." />
-            <span class="product-case"> {{ item.case }} </span>
-            <span class="product-discount"
-              >{{ item.discountPrice * 100 }}%</span
-            >
-            <button class="favorites"><i class="bi bi-suit-heart"></i></button>
-            <button class="details"><i class="bi bi-search"></i></button>
-            <button class="add-to-cart">Add To Cart</button>
-          </div>
-          <div class="product-info">
-            <div class="product-type">{{ item.type }}</div>
-            <ul class="product-colors">
-              <li>
-                <a></a>
-              </li>
-              <li>
-                <a></a>
-              </li>
-              <li>
-                <a></a>
-              </li>
-              <li>
-                <a></a>
-              </li>
-            </ul>
-            <div class="product-name">{{ item.name }}</div>
-            <div class="product-price">
-              <span class="sale-price"
-                >${{ (item.price * item.discountPrice).toFixed(2) }}</span
-              >
-              <span class="real-price">${{ item.price.toFixed(2) }}</span>
+      <div class="slider-buttons">
+        <div class="slider">
+          <div
+            class="cards"
+            :style="{ left: `calc(${left}% - ${cardIndex * 5}px)` }"
+          >
+            <div class="card" v-for="(item, index) in products" :key="index">
+              <div class="product-image">
+                <img :src="item.imageLink" alt="..." />
+                <span class="product-case"> {{ item.case }} </span>
+                <span class="product-discount"
+                  >{{ item.discountPrice * 100 }}%</span
+                >
+                <button class="favorites">
+                  <i class="bi bi-suit-heart"></i>
+                </button>
+                <button class="details"><i class="bi bi-search"></i></button>
+                <button class="add-to-cart">Add To Cart</button>
+              </div>
+              <div class="product-info">
+                <div class="product-type">{{ item.type }}</div>
+                <ul class="product-colors">
+                  <li>
+                    <a></a>
+                  </li>
+                  <li>
+                    <a></a>
+                  </li>
+                  <li>
+                    <a></a>
+                  </li>
+                  <li>
+                    <a></a>
+                  </li>
+                </ul>
+                <div class="product-name">{{ item.name }}</div>
+                <div class="product-price">
+                  <span class="sale-price"
+                    >${{ (item.price * item.discountPrice).toFixed(2) }}</span
+                  >
+                  <span class="real-price">${{ item.price.toFixed(2) }}</span>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-        <button class="btn-prev"><i class="bi bi-chevron-left"></i></button>
-        <button class="btn-next"><i class="bi bi-chevron-right"></i></button>
+        <button class="btn-prev">
+          <i class="bi bi-chevron-left" @click="slideLeft"></i>
+        </button>
+        <button class="btn-next" @click="slideRight">
+          <i class="bi bi-chevron-right"></i>
+        </button>
       </div>
     </div>
   </div>
@@ -69,7 +82,45 @@ export default {
       type: Array,
     },
   },
-  mounted() {},
+  data() {
+    return {
+      cardCountOnScreen: 4,
+      cardIndex: 0,
+      itemCountOnArray: this.products.length,
+      left: 0,
+    };
+  },
+  methods: {
+    slideLeft() {
+      if (this.cardIndex === 0) {
+        this.cardIndex = this.itemCountOnArray - this.cardCountOnScreen;
+        this.left -= 25 * this.cardIndex;
+      } else {
+        this.cardIndex--;
+        this.left += 25;
+      }
+    },
+    slideRight() {
+      if (this.itemCountOnArray - this.cardCountOnScreen === this.cardIndex) {
+        this.cardIndex = 0;
+        this.left = 0;
+      } else {
+        this.cardIndex++;
+        this.left -= 25;
+      }
+    },
+
+  },
+  mounted() {
+    setInterval(() => {
+        this.slideRight();
+      }, 5000);
+  },
+  // watch: {
+  //   cardIndex(value) {
+  //     console.log(value);
+  //   },
+  // },
 };
 </script>
 
@@ -117,14 +168,11 @@ $color-slide-button: #777777;
         }
       }
     }
-    .slider {
+    .slider-buttons {
+      // border: solid 1px blue;
       height: 100%;
       width: 100%;
-      margin-top: 30px;
-      display: flex;
-      gap: 20px;
       position: relative;
-      overflow: hidden;
       &:hover {
         > button {
           opacity: 1;
@@ -136,199 +184,214 @@ $color-slide-button: #777777;
           right: -50px;
         }
       }
-      .card {
-        border: solid 1px red;
+      .slider {
+        height: 100%;
         width: 100%;
-        display: flex;
-        flex-direction: column;
-        user-select: none;
-        .product-image {
-          position: relative;
-          cursor: pointer;
+        margin-top: 30px;
+        position: relative;
+        overflow: hidden;
+        .cards {
+          height: 100%;
+          width: 100%;
+          position: absolute;
+          top: 0;
           display: flex;
-          flex-direction: column;
-          &:hover {
-            .favorites,
-            .details {
-              transform: scale(1.5);
-              opacity: 1;
-            }
-            .add-to-cart {
-              opacity: 1;
-              bottom: 15px;
-            }
-          }
-          img {
-            width: 100%;
-            // object-fit: cover;
-          }
-          .product-case {
-            height: 22px;
-            width: 44px;
-            position: absolute;
-            top: 10px;
-            left: 10px;
+          gap: 20px;
+          transition: all 400ms ease;
+          .card {
+            min-width: calc(25% - 15px);
             display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: $color-gold;
-            color: $color-white;
-            border-radius: 14px;
-            font-size: 0.8rem;
-            cursor: default;
-          }
-          .product-discount {
-            height: 22px;
-            width: 44px;
-            position: absolute;
-            top: 40px;
-            left: 10px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            background-color: $color-black;
-            color: $color-white;
-            border-radius: 14px;
-            font-size: 0.8rem;
-            cursor: default;
-          }
-          .favorites {
-            height: 25px;
-            width: 25px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: absolute;
-            top: 20px;
-            right: 20px;
-            border: none;
-            border-radius: 50%;
-            background-color: white;
-            cursor: pointer;
-            transition: all ease 400ms;
-            opacity: 0;
-            color: #777777;
-
-            &:hover {
-              color: $color-gold;
-            }
-          }
-          .details {
-            height: 25px;
-            width: 25px;
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            position: absolute;
-            top: 65px;
-            right: 20px;
-            border: none;
-            border-radius: 50%;
-            background-color: white;
-            cursor: pointer;
-            transition: all ease 400ms;
-            opacity: 0;
-            color: #777777;
-            &:hover {
-              color: $color-gold;
-            }
-          }
-          .add-to-cart {
-            height: 40px;
-            width: 120px;
-            border: none;
-            border-radius: 20px;
-            cursor: pointer;
-            position: absolute;
-            bottom: 0;
-            align-self: center;
-            transition: all 400ms ease;
-            background-color: $color-white;
-            opacity: 0;
-            box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px,
-              rgba(0, 0, 0, 0.23) 0px 3px 6px;
-            &:hover {
-              background-color: $color-gold;
-              color: $color-white;
-            }
-          }
-        }
-        .product-info {
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-          gap: 15px;
-          padding: 10px 0;
-          .product-type {
-            color: #777777;
-            transition: 400ms all ease;
-            cursor: pointer;
-            &:hover {
-              color: $color-gold;
-            }
-          }
-          ul.product-colors {
-            margin: 0 !important;
-            display: flex;
-            gap: 5px;
-            li {
-              padding: 1px;
-              border-radius: 50%;
+            flex-direction: column;
+            user-select: none;
+            .product-image {
+              position: relative;
+              cursor: pointer;
               display: flex;
+              flex-direction: column;
+              &:hover {
+                .favorites,
+                .details {
+                  transform: scale(1.5);
+                  opacity: 1;
+                }
+                .add-to-cart {
+                  opacity: 1;
+                  bottom: 15px;
+                }
+              }
+              img {
+                width: 100%;
+                // object-fit: cover;
+              }
+              .product-case {
+                height: 22px;
+                width: 44px;
+                position: absolute;
+                top: 10px;
+                left: 10px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background-color: $color-gold;
+                color: $color-white;
+                border-radius: 14px;
+                font-size: 0.8rem;
+                cursor: default;
+              }
+              .product-discount {
+                height: 22px;
+                width: 44px;
+                position: absolute;
+                top: 40px;
+                left: 10px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background-color: $color-black;
+                color: $color-white;
+                border-radius: 14px;
+                font-size: 0.8rem;
+                cursor: default;
+              }
+              .favorites {
+                height: 25px;
+                width: 25px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                top: 20px;
+                right: 20px;
+                border: none;
+                border-radius: 50%;
+                background-color: white;
+                cursor: pointer;
+                transition: all ease 400ms;
+                opacity: 0;
+                color: #777777;
+
+                &:hover {
+                  color: $color-gold;
+                }
+              }
+              .details {
+                height: 25px;
+                width: 25px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                position: absolute;
+                top: 65px;
+                right: 20px;
+                border: none;
+                border-radius: 50%;
+                background-color: white;
+                cursor: pointer;
+                transition: all ease 400ms;
+                opacity: 0;
+                color: #777777;
+                &:hover {
+                  color: $color-gold;
+                }
+              }
+              .add-to-cart {
+                height: 40px;
+                width: 120px;
+                border: none;
+                border-radius: 20px;
+                cursor: pointer;
+                position: absolute;
+                bottom: 0;
+                align-self: center;
+                transition: all 400ms ease;
+                background-color: $color-white;
+                opacity: 0;
+                box-shadow: rgba(0, 0, 0, 0.16) 0px 3px 6px,
+                  rgba(0, 0, 0, 0.23) 0px 3px 6px;
+                &:hover {
+                  background-color: $color-gold;
+                  color: $color-white;
+                }
+              }
+            }
+            .product-info {
+              display: flex;
+              flex-direction: column;
               justify-content: center;
               align-items: center;
-              border: solid 1px gray;
-              transition: 400ms all ease;
-              &:nth-child(1) {
-                a {
-                  background-color: #b0c4de;
-                }
-              }
-              &:nth-child(2) {
-                a {
-                  background-color: #aa9e78;
-                }
-              }
-              &:nth-child(3) {
-                a {
-                  background-color: #808080;
-                }
-              }
-              &:nth-child(4) {
-                a {
-                  background-color: #964b00;
-                }
-              }
-              &:hover {
-                border: solid 1px $color-gold;
-              }
-              a {
-                border-radius: 50%;
-                display: inline-block;
-                height: 10px;
-                width: 10px;
+              gap: 15px;
+              padding: 10px 0;
+              .product-type {
+                color: #777777;
+                transition: 400ms all ease;
                 cursor: pointer;
+                &:hover {
+                  color: $color-gold;
+                }
               }
-            }
-          }
-          .product-name {
-            color: #555555;
-            cursor: pointer;
-            transition: all 400ms ease;
-            &:hover {
-              color: $color-gold;
-            }
-          }
-          .product-price {
-            display: flex;
-            gap: 10px;
-            .sale-price {
-              color: $color-gold;
-            }
-            .real-price {
-              color: #777777;
-              text-decoration: line-through;
+              ul.product-colors {
+                margin: 0 !important;
+                display: flex;
+                gap: 5px;
+                li {
+                  padding: 1px;
+                  border-radius: 50%;
+                  display: flex;
+                  justify-content: center;
+                  align-items: center;
+                  border: solid 1px gray;
+                  transition: 400ms all ease;
+                  &:nth-child(1) {
+                    a {
+                      background-color: #b0c4de;
+                    }
+                  }
+                  &:nth-child(2) {
+                    a {
+                      background-color: #aa9e78;
+                    }
+                  }
+                  &:nth-child(3) {
+                    a {
+                      background-color: #808080;
+                    }
+                  }
+                  &:nth-child(4) {
+                    a {
+                      background-color: #964b00;
+                    }
+                  }
+                  &:hover {
+                    border: solid 1px $color-gold;
+                  }
+                  a {
+                    border-radius: 50%;
+                    display: inline-block;
+                    height: 10px;
+                    width: 10px;
+                    cursor: pointer;
+                  }
+                }
+              }
+              .product-name {
+                color: #555555;
+                cursor: pointer;
+                transition: all 400ms ease;
+                &:hover {
+                  color: $color-gold;
+                }
+              }
+              .product-price {
+                display: flex;
+                gap: 10px;
+                .sale-price {
+                  color: $color-gold;
+                }
+                .real-price {
+                  color: #777777;
+                  text-decoration: line-through;
+                }
+              }
             }
           }
         }
